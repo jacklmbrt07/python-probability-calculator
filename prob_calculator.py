b4f1c078b8f642ceec42abf0feca450ff8e4d4fd
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-04 15:54:07 -0800

    hat defined

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,7 +3,15 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-    pass
+    def __init__(self, **kwargs) -> None:
+        self.contents = []
+        for key, value in kwargs.items():
+            i = 0
+            while i < value:
+                self.contents.append(key)
+                i += 1
+        
+        
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
